23064ff0ffcee5b7b42d6c509cf0654fd4580905
Diego Manso <diego.manso@gfi.world>
2020-05-09 15:30:16 +0200

    ThornTail labs

---
+++ cnd_thorntail_experienced/lab-01/src/test/resources/META-INF/test-load.sql
@@ -0,0 +1,3 @@
+insert into PRODUCT_INVENTORY (itemId, link, location, quantity) values ('123456', 'link', 'location', 99);
+insert into PRODUCT_INVENTORY (itemId, link, location, quantity) values ('987654', 'otro', 'lacalizacion', 69);
+insert into PRODUCT_INVENTORY (itemId, link, location, quantity) values ('147963', 'mas', 'localizacion', 33);
---
+++ cnd_thorntail_experienced/lab-03/src/main/java/com/redhat/coolstore/inventory/rest/HealthCheckResource.java
@@ -0,0 +1,19 @@
+package com.redhat.coolstore.inventory.rest;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@SuppressWarnings("deprecation")
+@Health
+@ApplicationScoped
+public class HealthCheckResource implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("server-state").up().build();
+    }
+
+}
